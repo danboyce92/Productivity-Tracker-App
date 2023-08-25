8f346312f6a158fcf79672732563e01d527eb683
Daniel B <daarf92@gmail.com>
2023-08-25 11:17:54 +0100

    Display percentage finished. Touch up responsiveness now

--- ProductivityTracker/src/components/Display/Display.tsx
+++ ProductivityTracker/src/components/Display/Display.tsx
@@ -13,11 +13,19 @@ export interface DProps {
 
 const Display: React.FC<DProps> = ({ records, barData }) => {
   const [thisWeekRecords, setThisWeekRecords] = useState<Record[]>([]);
+  const [lastWeekRecords, setLastWeekRecords] = useState<Record[]>([]);
 
   const calculateLastWeek = () => {
     const now = Date.now();
     const lastWeek = now - 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
     return records.filter(record => record.timestamp > lastWeek);
+  }
+
+  const calculateTwoWeeksAgo = () => {
+    const now = Date.now();
+    const lastWeek = now - 604800000;
+    const twoWeeksAgo = now - (604800000 * 2);
+    return records.filter(record => record.timestamp > twoWeeksAgo && record.timestamp < lastWeek);
   }
 
 
@@ -27,11 +35,13 @@ const Display: React.FC<DProps> = ({ records, barData }) => {
   useEffect(() => {
     const filteredRecords = calculateLastWeek();
     setThisWeekRecords(filteredRecords);
+    const lastFilteredRecords = calculateTwoWeeksAgo();
+    setLastWeekRecords(lastFilteredRecords);
   }, [records])
 
   return (
     <div id="display-container">
-      <div id="percent-disp"><PercentageComponent /></div>
+      <div id="percent-disp"><PercentageComponent thisWeekR={thisWeekRecords} lastWeekR={lastWeekRecords} /></div>
       <div id="donut-disp"><DonutWrapper records={thisWeekRecords} /></div>
       <div id="bar-disp"><BarWrapper barData={barData} /></div>
     </div>
--- ProductivityTracker/src/components/Display/PercentageComponent.tsx
+++ ProductivityTracker/src/components/Display/PercentageComponent.tsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Record } from '../Activities';
 
-const PercentageComponent: React.FC = () => {
+
+interface PercentageProps {
+  thisWeekR: Record[]
+  lastWeekR: Record[]
+}
+
+const PercentageComponent: React.FC<PercentageProps> = ({ thisWeekR, lastWeekR }) => {
+  const [percentage, setPercentage] = useState<number>(0)
+  const [posOrNeg, setPosOrNeg] = useState('black');
+
+  const totalTimeAccumulator = (thisWeek: Record[], lastWeek: Record[]) => {
+    let thisWeekTotal = 0;
+    let lastWeekTotal = 0;
+    
+    for (let record of thisWeek) {
+      thisWeekTotal += record.duration;
+    }
+    for (let record of lastWeek) {
+      lastWeekTotal += record.duration;
+    }
+
+    const difference = thisWeekTotal - lastWeekTotal;
+    if (lastWeekTotal === 0) {
+      if (difference > 0) {
+        return Infinity; // First number is infinitely higher
+      } else if (difference < 0) {
+        return -Infinity; // Second number is infinitely higher
+      } else {
+        return 0; // Both numbers are equal
+      }
+    }
+    if (difference > 0) {
+      setPosOrNeg('green');
+    }
+    if (difference < 0) {
+      setPosOrNeg('red');
+    }
+
+    const percentage = (difference / lastWeekTotal) * 100;
+    return percentage;
+
+  }
+
+  useEffect(() => {
+    
+    setPercentage(totalTimeAccumulator(thisWeekR, lastWeekR));
+    console.log(percentage)
+  },[thisWeekR])
+
+
   return (
-    <div></div>
+    <div>
+      <div id="text">Week-to-Week Progress</div>
+      <div className={`text-${posOrNeg}`} id="percentage">{percentage}%</div>
+    </div>
   )
 }
 
