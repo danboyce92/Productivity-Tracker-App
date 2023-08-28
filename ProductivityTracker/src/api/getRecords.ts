63acfd8d6ed8eb36c7803e19f9dbe2e64fc25de6
Daniel B <daarf92@gmail.com>
2023-08-28 11:51:49 +0100

    proxy attempt

--- ProductivityTracker/src/api/getRecords.ts
+++ ProductivityTracker/src/api/getRecords.ts
@@ -1,6 +1,8 @@
 import { url } from "./config";
+//@ts-ignore
+import proxy from '../../vercel/functions/proxy.js';
 
 export async function getRecords() {
-  const response = await fetch(`${url}:7000/records`)
+  const response = await fetch(`${proxy}`)
   return response.json();
 }
