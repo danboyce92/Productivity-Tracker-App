7648af5da5a113e6828783e577e310bed4a634d9
Daniel B <daarf92@gmail.com>
2023-08-28 09:42:52 +0100

    trying with mongodbURI

--- ProductivityTracker/src/api/config.ts
+++ ProductivityTracker/src/api/config.ts
@@ -1,1 +1,1 @@
-export const url = 'http://51.103.71.229';
+export const url = 'http://mongodb+srv://vercel-admin-user-64ec5a81a6a62349de4ea534:ubep3qLYmXYDeioQ@cluster1.v4aquyg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
